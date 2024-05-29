cbef6a0ff8b3ee02182e715b30402faf3fa8dee8
hoanghuycoder <21130386@st.hcmuaf.edu.vn>
2024-05-29 18:23:50 +0700

    feat : upload video

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+import cors from 'cors';
+const jwt = require('jsonwebtoken');
+const app = express();
 const port = 8000;
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
